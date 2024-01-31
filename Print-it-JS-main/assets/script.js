4b53a9aa0bcb9b65204602e8237176fa8aaf30af
Constantin-D <ctono75@hotmail.com>
2024-01-31 17:52:08 +0100

    configuring steps of the project

--- Print-it-JS-main/assets/script.js
+++ Print-it-JS-main/assets/script.js
@@ -1,53 +1,4 @@
 // Tableau
-
-// const slides = [
-// 	{
-// 		"image":"slide1.jpg",
-// 		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-// 	},
-// 	{
-// 		"image":"slide2.jpg",
-// 		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-// 	},
-// 	{
-// 		"image":"slide3.jpg",
-// 		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-// 	},
-// 	{
-// 		"image":"slide4.png",
-// 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-// 	}
-// ];
-
-// const dots = document.querySelector(".dots")
-// // arrow_lef & arrow_right
-// let selectedDot =0
-// const arrowRight = document.querySelector(".arrow_right")
-// const arrowLeft = document.querySelector(".arrow_left")
-// const slidesLength = slides.length
-// console.log(slidesLength);
-
-// function dotsGenerate() {
-// 	for(let i=0; i<slidesLength; i++){
-// 		const divDot = document.createElement("div")
-// 		divDot.setAttribute("class", "dot")
-// 		dots.appendChild(divDot)
-// 		if (i === selectedDot) {
-// 			divDot.setAttribute("class", "dot_selected")
-// 		}
-// 	}
-// }
-
-// dotsGenerate()
-
-// arrowRight.addEventListener("click", function(event){
-// 	console.log(event)
-// })
-
-// arrowLeft.addEventListener("click", function(event){
-// 	console.log(event)
-// })
-
 const slides = [
     {
         image: "slide1.jpg",
@@ -105,20 +56,16 @@ arrowLeft.addEventListener("click", (e) => {
 function updateSlide() {
     // Changer l'image
     const imagePath = "./assets/images/slideshow/" + slides[selectedDot].image;
-    console.log(imagePath);
     const imageElement = document.querySelector(".banner-img");
-    console.log(imageElement);
     imageElement.src = imagePath;
 
     // Changer le texte correspondant à l'image
     const textElement = document.querySelector("#banner p");
-    console.log(textElement);
     // Mettre à jour le texte de l'image affichée
     textElement.innerHTML = slides[selectedDot].tagLine;
 
-    // Mettre à jour les points indicateurs
+    // Mettre à jour les points indicateurs (dot-solid)
     const dots = document.querySelectorAll(".dot");
-    console.log(dots);
     dots.forEach((dot, index) => {
         if (index === selectedDot) {
             dot.classList.add("dot_selected");
@@ -134,6 +81,7 @@ function updateSlide() {
 arrowRight.addEventListener("click", (e) => {
     // Changement droit du dot sélectionné
     selectedDot = (selectedDot + 1) % slidesLength;
+    // console.log(e);
     updateSlide();
 });
 
@@ -142,6 +90,7 @@ arrowRight.addEventListener("click", (e) => {
 arrowLeft.addEventListener("click", (e) => {
     // Changement gauche du dot sélectionné
     selectedDot = (selectedDot - 1 + slidesLength) % slidesLength;
+    // console.log(e);
     updateSlide();
 });
 
